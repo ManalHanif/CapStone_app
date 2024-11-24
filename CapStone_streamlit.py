375d35a2d95e56392ddb6e66a29807a3dd34b791
ManalHanif <manalhanif24@gmail.com>
2024-11-24 22:39:38 +0300

    Update CapStone_streamlit.py

--- CapStone_streamlit.py
+++ CapStone_streamlit.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 
 # Raw URL of your CSV file from GitHub
-csv_url = "https://raw.githubusercontent.com/ManalHanif/CapStone_app/main/youtube_data_eda_2 (1).csv"  # Replace with your actual file path
+csv_url = "https://github.com/ManalHanif/CapStone_app/blob/main/youtube_data_eda_2.csv"  # Replace with your actual file path
 
 st.title("Display DataFrame from GitHub CSV")
 
