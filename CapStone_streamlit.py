31146129f320d2836381a8299f96f60df8e7a20a
ManalHanif <manalhanif24@gmail.com>
2024-11-25 00:41:07 +0300

    Update CapStone_streamlit.py

--- CapStone_streamlit.py
+++ CapStone_streamlit.py
@@ -4,5 +4,12 @@ import pandas as pd
 # Raw URL of your CSV file from GitHub
 df=pd.read_csv("youtube_data_eda_2.csv")# Replace with your actual file path
 
-st.title("Display DataFrame from GitHub CSV")
+st.title("رائج")
+st.write("""
+<div style='text-align: right; direction: rtl;'>
+    قد فكرت في يوم في القنوات اللي بتحقق أعلى نسب مشاهدة في السعودية؟ 
+    من اللي ورا نجاحها؟ كم مصدر دخلهم؟ يوتيوب ما صار مجرد منصة ترفيهية؛ 
+    اليوم هو المكان اللي يجمع كل شيء: الضحك، الدراما، التعليم، وحتى القصص اللي تعيشها كأنها واقع.
+</div>
+""", unsafe_allow_html=True)
 df
